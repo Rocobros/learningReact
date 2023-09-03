c60c5a0be66bd7bdf5a73878a0f22d7f4e765908
Rodrigo Romero <rocobros21@gmail.com>
2023-09-02 20:26:03 -0600

    Finished project 00

---
+++ projects/00-twitter-card/src/TwitterFollowCard.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+
+export function TwitterFollowCard ({ userName, children, initialIsFollowing }) {
+
+    const [ isFollowing, setIsFollowing ] = useState(initialIsFollowing) 
+    const buttonText = isFollowing ? 'Siguiendo' : 'Seguir'
+    const buttonClassName = isFollowing ? 'tw-followCard-button is-following' : 'tw-followCard-button'
+
+    const handleClick = () => {
+        setIsFollowing(!isFollowing)
+    }
+    return(
+    <article className='tw-followCard'>
+            <header className='tw-followCard-header'>
+                <img className='tw-followCard-avatar' src={`https://unavatar.io/${userName}`}/>
+                <div className='tw-followCard-info'>
+                    <strong>{children}</strong>
+                    <span className='tw-followCard-infoUserName'>{`@${userName}`}</span>
+                </div>
+            </header>
+
+            <aside>
+                <button onClick={handleClick} className={buttonClassName}>
+                    <span className="tw-followCard-text">{buttonText}</span>
+                    <span className="tw-followCard-stopFollow">Dejar de seguir</span>
+                </button>
+            </aside>
+        </article>
+    )
+}
